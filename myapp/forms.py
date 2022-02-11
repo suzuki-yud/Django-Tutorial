ae6a7898fb2b1bbd132e742080380e2005a73af5
suzuki-yud <suzuki.yud1028@gmail.com>
2022-02-11 15:57:46 +0900

    Arranged the page to create new post by Bootstrap

--- myapp/forms.py
+++ myapp/forms.py
@@ -6,3 +6,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
