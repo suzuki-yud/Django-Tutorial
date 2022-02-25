82cf07e4163df0b975da53995bfc35fb9892454b
suzuki-yud <suzuki.yud1028@gmail.com>
2022-02-25 17:03:38 +0900

    Edited setting.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+env.read_env('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yp43&0p-q7@k=ul26fd9%mn(_jg5_=aqjsfbfcv(re-8zh_&hm'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG',default=True)
 
 ALLOWED_HOSTS = []
 
